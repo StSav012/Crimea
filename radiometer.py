f9b08619dc06979f0f70ea255368594a99d42225
StSav012 <stsav012@gmail.com>
2020-07-28 11:49:12 +0300

    Attempt to re-start ADC measurement process

--- radiometer.py
+++ radiometer.py
@@ -12,6 +12,7 @@ class ADC(Thread):
         self.timeout: float = timeout
         self._channels: List[int] = channels[:]
         self._channels_str: List[str] = [str(channel) for channel in self._channels]
+        self._app: str = app
         self.voltages: List[float] = [nan] * len(self._channels)
         if max(self._channels) > 7:
             raise ValueError(f'There is no channel {max(self._channels)}')
@@ -24,17 +25,20 @@ class ADC(Thread):
     def run(self):
         self._is_running = True
         try:
-            while self._is_running and self._p.poll() is None:
-                for index, channel in enumerate(self._channels_str):
-                    self._p.stdin.write((channel + '\n').encode('ascii'))
-                    self._p.stdin.flush()
-                    r = self._p.stdout.readline().strip().decode('ascii').split(maxsplit=1)
-                    if r and r[0] == channel:
-                        self.voltages[index] = float(r[1])
-                    else:
-                        self.voltages[index] = nan
-                time.sleep(self.timeout)
-            if not self._is_running:
+            while self._is_running:
+                while self._is_running and self._p.poll() is None:
+                    for index, channel in enumerate(self._channels_str):
+                        self._p.stdin.write((channel + '\n').encode('ascii'))
+                        self._p.stdin.flush()
+                        r = self._p.stdout.readline().strip().decode('ascii').split(maxsplit=1)
+                        if r and r[0] == channel:
+                            self.voltages[index] = float(r[1])
+                        else:
+                            self.voltages[index] = nan
+                    time.sleep(self.timeout)
+                if self._is_running:
+                    self._p = Popen([self._app, str(max(self._channels) + 1)], stdin=PIPE, stdout=PIPE)
+            else:
                 self._p.communicate(input=b'-1', timeout=3)
         except (KeyboardInterrupt, SystemExit):
             self._p.communicate(input=b'-1', timeout=3)
