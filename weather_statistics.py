eb9d8691470b1e14f074c3b7ac0b394f13ec3220
StSav012 <stsav012@gmail.com>
2021-04-23 21:28:30 +0300

    Added weather statistics to rp5 data

--- weather_statistics.py
+++ weather_statistics.py
@@ -4,6 +4,8 @@
 # sending email
 import calendar
 import sys
+from datetime import timedelta
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -84,8 +86,10 @@ def main():
 
     for start_month, end_month in ([5, 9], [11, 3]):
         if end_month >= start_month:
+            # noinspection PyTypeChecker
             mask: np.ndarray = ((data.index.month >= start_month) & (data.index.month <= end_month))
         else:
+            # noinspection PyTypeChecker
             mask: np.ndarray = ((data.index.month >= start_month) | (data.index.month <= end_month))
         if not mask.any():
             continue
@@ -150,5 +154,61 @@ def main():
                                          f'to {calendar.month_name[end_month]} of any year 0AM to 4AM.txt', sep='\t')
 
 
+def match_rp5():
+    an_hour_and_a_half: timedelta = timedelta(minutes=90)
+    data: pd.DataFrame = pd.read_table(sys.argv[1],
+                                       index_col='Time',
+                                       parse_dates=True,
+                                       usecols=[
+                                           'Time',
+                                           'Barometer',
+                                           # 'InsideTemp',
+                                           # 'InsideHum',
+                                           'OutsideTemp',
+                                           'WindSpeed',
+                                           'AvgWindSpeed',
+                                           # 'WindDir',
+                                           'OutsideHum',
+                                           'RainRate',
+                                           'UVLevel',
+                                           'SolarRad',
+                                           'StormRain',
+                                           'RainDay',
+                                           # 'RainMonth',
+                                           # 'RainYear',
+                                           'ETDay',
+                                           # 'ETMonth',
+                                           # 'ETYear',
+                                           # 'BattLevel',
+                                           # 'Sunrise',
+                                           # 'Sunset',
+                                           # 'Forecast'
+                                       ]
+                                       )
+    rp5_data: pd.DataFrame = pd.read_table(sys.argv[2],
+                                           index_col=0,
+                                           sep=';',
+                                           comment='#'
+                                           )
+    # noinspection PyTypeChecker
+    rp5_data.index = pd.to_datetime(rp5_data.index, format='%d.%m.%Y %H:%M')
+    rp5_data = rp5_data.dropna(axis=1, how='all')
+    averaged_data: Optional[pd.DataFrame] = None
+    for rp5_ts, rp5_line in rp5_data.iterrows():
+        print(rp5_ts)
+        # noinspection PyTypeChecker
+        data_piece: pd.DataFrame = data.loc[(rp5_ts - an_hour_and_a_half < data.index)
+                                            & (data.index < rp5_ts + an_hour_and_a_half)]
+        if averaged_data is None:
+            averaged_data = pd.DataFrame(data_piece.median(), columns=[rp5_ts])
+        else:
+            averaged_data = pd.concat((averaged_data, pd.DataFrame(data_piece.median(), columns=[rp5_ts])), axis=1)
+    total_averaged_data: pd.DataFrame = pd.concat((averaged_data.T, rp5_data), axis=1)
+    total_averaged_data.to_excel('/tmp/rp5 comparison.xlsx', sheet_name='Weather', freeze_panes=(1, 1))
+
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        main()
+    elif len(sys.argv) == 3:
+        match_rp5()
